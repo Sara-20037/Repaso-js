69ca15e74f77e6d9fa07138c47ddd437d95fc96d
Sara <111981426+Sara-20037@users.noreply.github.com>
2023-02-22 08:47:39 -0500

    Delete Act1 directory

--- Act1/Ejercicio 1/ejercicio1.js
+++
@@ -1,9 +0,0 @@
-let dato = prompt("Ingrese numero")
- function operacion(){
-    if (dato%2 === 0){
-        alert("es divisible")
-    } else{
-        alert("no es divisible")
-    }
- }
- operacion();
--- Act1/Ejercicio 2/ejercicio2.js
+++
@@ -1,9 +0,0 @@
-let dato = prompt("Ingrese numero")
- function operacion(){
-    if (dato%2 === 0){
-        alert("es par")
-    } else{
-        alert("no impar")
-    }
- }
- operacion();
